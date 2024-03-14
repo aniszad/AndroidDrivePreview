505ba1d02d3464508767d5d0aaa4d1d09f56c82b
aniszad <91326965+aniszad@users.noreply.github.com>
2024-03-14 10:58:46 +0100

    fixing gradle jdk and gradle version compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jetbrains.kotlin.fir.declarations.builder.buildScript
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+    }
+}
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
